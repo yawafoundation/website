db3ff03f341284e63e9be9dab90ab720a8a007be
Zaim Bakar <37629946+zzzaim@users.noreply.github.com>
2020-03-07 16:02:15 +0800

    feat: use nextjs and react frameworks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,27 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   },
   ignorePatterns: ["public/", "!*rc.js"],
+  plugins: ["react"],
   rules: {}
 };
